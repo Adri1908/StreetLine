8e868b2cf2a6dca5a2df17f82adb18e2654d8152
MMMatth <mat.loup04@gmail.com>
2021-12-09 11:42:25 +0100

    j'ai change le test

--- trait.py
+++ trait.py
@@ -7,7 +7,7 @@ def trait(x1,y1,x2,y2):
         x2, y2 : coordonnées de la fin du trait
     Cette function dessine un trait entre les 2 points transmis en paramètres
     '''
-    print("It's a test")
+    print("test")
 
 if __name__ == '__main__':
     trait(0,0,100,100)
