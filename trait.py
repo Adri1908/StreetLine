83c6d2133afa5b239830e00a9b0efb510c3d32c2
ThomasMrchl <marchalthomas88@gmail.com>
2021-12-13 09:30:13 +0100

    trait correct

--- trait.py
+++ trait.py
@@ -7,17 +7,11 @@ def trait(x1,y1,x2,y2):
         x2, y2 : coordonnées de la fin du trait
     Cette function dessine un trait entre les 2 points transmis en paramètres
     '''
-<<<<<<< HEAD
     turtle.up()
     turtle.goto(x1,y1)
     turtle.down()
     turtle.goto(x2,y2)
     turtle.up()
-=======
-    print("test 100")
-#KILIAN PUE LA GROSSE MERD*
-
->>>>>>> cdcb0850c33234d49ded58f855c9a52ce998f0c7
 if __name__ == '__main__':
     trait(0,0,100,100)
     
