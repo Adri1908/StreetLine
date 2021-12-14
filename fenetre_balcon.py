aa448f419fcf71fc1355407d978c93fab0fda4ce
kylian-project <kylian88100@gmail.com>
2021-12-14 08:20:43 +0100

    fenetre balcon

--- fenetre_balcon.py
+++ fenetre_balcon.py
@@ -1,3 +1,4 @@
+from turtle import *
 import turtle
 from rectangle import rectangle
 from trait import trait
@@ -12,16 +13,42 @@ def fenetre_balcon(x,y):
         puis le balcon
     '''
     # porte-fenetre
-
-    pass
-
+    color('black', 'lightgrey')
+    begin_fill()
+    for i in range (2):
+        forward(30)
+        right(90)
+        forward(50)
+        right(90)
+    end_fill()
+    up()
 
     # balcon
-
-
-    pass
-
-
+    turtle.pensize(3)
+    right(90)
+    forward(25)
+    right(90)
+    forward(5)
+    right(180)
+    down()
+    for i in range(2):
+        forward(40)
+        right(90)
+        forward(25)
+        right(90)
+    
+    for i in range(3):
+        forward(5)
+        right(90)
+        forward(25)
+        left(90)
+        forward(5)
+        left(90)
+        forward(25)
+        right(90)
+    forward(5)
+    right(90)
+    forward(25)
 
 if __name__ == '__main__':
     fenetre_balcon(0,0)
