c0f6a3d171bb63ab643fe1c7b679591afe9bf621
ThomasMrchl <marchalthomas88@gmail.com>
2021-12-14 08:43:36 +0100

    ok

--- facade.py
+++ facade.py
@@ -11,18 +11,10 @@ def facade(x, y_sol, couleur, niveau):
     remarque :
         Facade dessine une facade sans les élements interieurs
     '''
-<<<<<<< HEAD
-    turtle.fillcolor(couleur)  #Change la couleur de remplissage à rouge
-    turtle.begin_fill()  #Précise le début du remplissage
-    rectangle(x,y_sol + 60 * niveau,140,60)
-    turtle.end_fill() 
-=======
-   
     turtle.fillcolor("red")
     turtle.begin_fill()
     rectangle(x, y_sol, 140, 60)
     turtle.end_fill()
->>>>>>> e87704d4003995e44baabdd81d4ae385d54c9d9f
 
 
 
