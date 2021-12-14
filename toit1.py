ea7b20ccc57805381de4b6920f4a0bc540455061
Beaudeux <f_beaudeux@yahoo.fr>
2021-12-14 09:02:32 +0100

    toit1 fait

--- toit1.py
+++ toit1.py
@@ -9,8 +9,21 @@ def toit1(x, y_sol, niveau):
     Cette fonction dessine un toit triangulaire noir de base 160 pixels
     et de hauteur centrale 40 pixels
     '''
-    pass
+    turtle.up()
+    y = y_sol+60*niveau
+    turtle.goto(x,y)
 
+    turtle.down()
+    turtle.forward(80)
+
+    turtle.left(153.44)
+    turtle.forward(89.44)
+
+    turtle.left(53.12)
+    turtle.forward(89.44)
+
+    turtle.left(153.44)
+    turtle.forward(80)
 
 if __name__ == '__main__':
     toit1(0,0,0)
