3cc262d0e57bf6db53d0ec1742bdfcf81dbed773
Adri1 <adrienlecacheux@gmail.com>
2024-11-23 12:25:14 +0100

    Optimization (remove hard coding)
    Optimization using a for loop instand of hard coded if statements

--- etage.py
+++ etage.py
@@ -17,22 +17,35 @@ def etage(x, y_sol, couleur, niveau):
     # dessin des murs
 
     # dessin des 3 Eléments
-    facade(x, y_sol, couleur, niveau)
-    i = randint(1,2)
-    if i == 1 :
-        fenetre(x+15,y_sol+60*niveau+15)
-    else : fenetre_balcon(x+15,y_sol+60*niveau+55)
+    # facade(x, y_sol, couleur, niveau)
+    # i = randint(1,2)
+    # if i == 1 :
+    #     fenetre(x+15,y_sol+60*niveau+15)
+    # else : fenetre_balcon(x+15,y_sol+60*niveau+55)
 
-    i = randint(1,2)
-    if i == 1 :
-        fenetre(x+60,y_sol+60*niveau+15)
-    else : fenetre_balcon(x+60,y_sol+60*niveau+55)
+    # i = randint(1,2)
+    # if i == 1 :
+    #     fenetre(x+60,y_sol+60*niveau+15)
+    # else : fenetre_balcon(x+60,y_sol+60*niveau+55)
     
-    i = randint(1,2)
-    if i == 1 :
-        fenetre(x+105,y_sol+60*niveau+15)
-    else : fenetre_balcon(x+105,y_sol+60*niveau+55)
+    # i = randint(1,2)
+    # if i == 1 :
+    #     fenetre(x+105,y_sol+60*niveau+15)
+    # else : fenetre_balcon(x+105,y_sol+60*niveau+55)
     
+    
+    facade(x, y_sol, couleur, niveau)
+
+    # Coordinates for the windows or balconies
+    positions = [15, 60, 105]
+
+    for pos in positions:
+        i = randint(1, 2)
+        if i == 1:
+            fenetre(x + pos, y_sol + 60 * niveau + 15)
+        else:
+            fenetre_balcon(x + pos, y_sol + 60 * niveau + 55)
+
         
     
 
