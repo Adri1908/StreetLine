f4ff2870bb5f0a66b4c627b31b395540a2cdfb72
Adri1 <adrienlecacheux@gmail.com>
2024-11-30 15:07:55 +0100

    Fix etage module

--- etage.py
+++ etage.py
@@ -15,39 +15,18 @@ def etage(x, y_sol, couleur, niveau):
        Cette fonction dessine un étage d'un immeuble
     '''
     # dessin des murs
-
-    # dessin des 3 Eléments
-    # facade(x, y_sol, couleur, niveau)
-    # i = randint(1,2)
-    # if i == 1 :
-    #     fenetre(x+15,y_sol+60*niveau+15)
-    # else : fenetre_balcon(x+15,y_sol+60*niveau+55)
-
-    # i = randint(1,2)
-    # if i == 1 :
-    #     fenetre(x+60,y_sol+60*niveau+15)
-    # else : fenetre_balcon(x+60,y_sol+60*niveau+55)
-    
-    # i = randint(1,2)
-    # if i == 1 :
-    #     fenetre(x+105,y_sol+60*niveau+15)
-    # else : fenetre_balcon(x+105,y_sol+60*niveau+55)
-    
-    
     facade(x, y_sol, couleur, niveau)
 
-    # Coordinates for the windows or balconies
-    positions = [15, 60, 105]
+   # Coordonnées des positions des 3 éléments
+    positions = [10, 55, 100]
 
+    # dessin des 3 Eléments
     for pos in positions:
         i = randint(1, 2)
         if i == 1:
             fenetre(x + pos, y_sol + 60 * niveau + 15)
         else:
-            fenetre_balcon(x + pos, y_sol + 60 * niveau + 55)
-
-        
-    
+            fenetre_balcon(x + pos, y_sol + 60 * niveau + 55)     
 
 if __name__ == '__main__':
     etage(0,0,"red",0)
